f82ceef84f61a1b015da1ad8155d959c3f0426e0
Karina Miranda UTN <Karina.Miranda@frt.utn.edu.ar>
2026-03-11 20:02:05 -0300

    problema 7 resuelto

--- js/7-tresnumeros.js
+++ js/7-tresnumeros.js
@@ -1,12 +1,7 @@
 const num1 = parseInt(prompt("Ingresa el primer número:"));
 const num2 = parseInt(prompt("Ingresa el segundo número:"));
 const num3 = parseInt(prompt("Ingresa el tercer número:"));
+
 document.writeln(
-  `El número mayor entre ${num1} y ${num2} es: ${Math.max(num1, num2)}`,
-);
-document.writeln(
-  `El número mayor entre ${num2} y ${num3} es: ${Math.max(num2, num3)}`,
-);
-document.writeln(
-  `El número mayor entre ${num1} y ${num3} es: ${Math.max(num1, num3)}`,
+  `El mayor de los tres números (${num1}, ${num2}, ${num3}) es: ${Math.max(num1, num2, num3)}`,
 );
